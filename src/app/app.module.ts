4344313932b1e7936491e50e96aea41a274dbdec
Fedia Haamdi <hamdi.fedia@i-360.tn>
2022-03-08 21:22:24 +0100

    initialization of the product section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './features/home/home.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './features/home/product/product.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
+    HomeComponent,
+    ProductComponent,
     AppComponent,
     NavbarComponent,
     FooterComponent
